7b6370c9e7de953f77387a2567b3dc91fec5f55f
jjpush <lj845854@gmail.com>
2023-03-28 13:21:03 +0900

    feat: Mode Protocol 정의
    - 타입 정의에 필요한 ModeType 열거형 정의
    - ModeType은 Domain 영역에서 사용되는 모델이 채택해서 공유하므로 Common 폴더에 위치

---
+++ Tempus/Domain/Common/ModeType.swift
@@ -0,0 +1,14 @@
+//
+//  ModeType.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/03/28.
+//
+
+import Foundation
+
+enum ModeType: Int {
+    case timer = 1
+    case block = 2
+    case daily = 3
+}
