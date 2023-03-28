3960b5a34a3b4c72032307952d919a4ac18f8c6c
jjpush <lj845854@gmail.com>
2023-03-29 05:13:14 +0900

    feat: DataRepository 프로토콜 정의
    - Mode Protocol 삭제.

---
+++ Tempus/Domain/Repository/DataRepository.swift
@@ -0,0 +1,26 @@
+//
+//  DataRepository.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/03/29.
+//
+
+import Foundation
+
+protocol DataRepository {
+    func create(mode: TimerModel) throws
+    func create(mode: BlockModel) throws
+    func create(mode: DailyModel) throws
+    
+    func fetch() throws -> [TimerModel]
+    func fetch() throws -> [BlockModel]
+    func fetch() throws -> [DailyModel]
+    
+    func update(_ mode: TimerModel) throws
+    func update(_ mode: BlockModel) throws
+    func update(_ mode: DailyModel) throws
+    
+    func delete(_ model: TimerModel) throws
+    func delete(_ model: BlockModel) throws
+    func delete(_ model: DailyModel) throws
+}
