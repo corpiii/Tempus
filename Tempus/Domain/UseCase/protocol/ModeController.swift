f46446e7483be468e79ae4014bacde44d1728839
jjpush <lj845854@gmail.com>
2023-04-05 13:08:47 +0900

    feat: Mode를 정지, 시작할 수 있는 modeController 프로토콜 정의

---
+++ Tempus/Domain/UseCase/protocol/ModeController.swift
@@ -0,0 +1,15 @@
+//
+//  ModeController.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/04/04.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol modeController {    
+    func modeStart()
+    func modeStop()
+}
