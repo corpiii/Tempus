03aa13e8f2453ff4d358a17073319302405057e9
jjpush <lj845854@gmail.com>
2023-04-21 19:32:33 +0900

    refactor: BlockFetchUseCase가 변함에 따라 testcase 수정

--- TempusTests/UseCaseTests/BlockUseCaseTests/BlockFetchUseCaseTest.swift
+++ TempusTests/UseCaseTests/BlockUseCaseTests/BlockFetchUseCaseTest.swift
@@ -7,15 +7,19 @@
 
 import XCTest
 
+import RxSwift
+
 final class BlockFetchUseCaseTest: XCTestCase {
     var blockCreateUseCase: BlockCreateUseCase!
     var blockFetchUseCase: BlockFetchUseCase!
     var coreDataRepositoryMock: DataManagerRepositoryMock!
+    var disposeBag: DisposeBag!
     
     override func setUpWithError() throws {
         coreDataRepositoryMock = DataManagerRepositoryMock()
         blockCreateUseCase = BlockCreateUseCase(repository: coreDataRepositoryMock)
         blockFetchUseCase = BlockFetchUseCase(repository: coreDataRepositoryMock)
+        disposeBag = DisposeBag()
     }
     
     func test_Block_fetch_is_success() {
@@ -24,16 +28,34 @@ final class BlockFetchUseCaseTest: XCTestCase {
         let title = "testTitle"
         let divideCount = 6
         let model = BlockModel(id: id, title: title, divideCount: divideCount)
+        let expectation = XCTestExpectation(description: "fetch_usecase_test")
         
-        try! blockCreateUseCase.execute(model: model) {}
+        let fetchEvent: PublishSubject<Void> = .init()
+        let createEvent: PublishSubject<BlockModel> = .init()
+        
+        let createInput = BlockCreateUseCase.Input(modelFetchEvent: fetchEvent, modelCreate: createEvent)
+        let createOutput = blockCreateUseCase.transform(input: createInput, disposeBag: disposeBag)
+        
+        createOutput.isCreateSuccess
+            .subscribe(onNext: { isSuccess in
+                XCTAssertEqual(isSuccess, true)
+            }).disposed(by: disposeBag)
+        createEvent.onNext(model)
         
         // Act, Assert
-        do {
-            try blockFetchUseCase.execute { models in
-                XCTAssertEqual(models.first!.id, id)
-            }
-        } catch {
-            XCTFail()
-        }
+        let fetchInput = BlockFetchUseCase.Input(fetchModelEvent: fetchEvent)
+        let fetchOutput = blockFetchUseCase.transform(input: fetchInput, disposeBag: disposeBag)
+        
+        fetchOutput.modelArrayObservable
+            .subscribe(onNext: { models in
+                if !models.isEmpty {
+                    XCTAssertEqual(models.first!.id, model.id)
+                    expectation.fulfill()
+                }
+            }).disposed(by: disposeBag)
+        
+        fetchEvent.onNext(())
+        
+        wait(for: [expectation], timeout: 1.0)
     }
 }
