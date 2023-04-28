d4379ce65ac0437fe176ef4654a84edf192eab33
jjpush <lj845854@gmail.com>
2023-04-28 20:24:07 +0900

    feat: DailyListViewModel Input, Output 정의

---
+++ Tempus/Presentation/DailyScene/ViewModel/DailyListViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  DailyListViewModel.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/04/28.
+//
+
+import RxSwift
+
+final class DailyListViewModel {
+    struct Input {
+        let addButtonEvent: Observable<Void>
+        let modelDeleteEvent: Observable<DailyModel>
+        let modelFetchEvent: PublishSubject<Void>
+    }
+    
+    struct Output {
+        let blockModelArray: Observable<[DailyModel]>
+        let isDeleteSuccess: Observable<Bool>
+    }
+}
