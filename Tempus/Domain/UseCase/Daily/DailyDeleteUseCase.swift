b5ff5205fdfb90f7fe28e4e61c7abbcd2c714d33
jjpush <lj845854@gmail.com>
2023-04-29 06:00:30 +0900

    feat: DailyListViewModel 의 Input, Output 및 bind 메서드 정의
    - ListViewModel에서 사용하는 DailyDeleteUseCase, DailyFetchUseCase 또한 Input, Output 작업

--- Tempus/Domain/UseCase/Daily/DailyDeleteUseCase.swift
+++ Tempus/Domain/UseCase/Daily/DailyDeleteUseCase.swift
@@ -5,12 +5,47 @@
 //  Created by 이정민 on 2023/04/02.
 //
 
+import RxSwift
+
 final class DailyDeleteUseCase {
-    let repository: DataManagerRepository
+    struct Input {
+        let modelDeleteEvent: Observable<DailyModel>
+    }
+    
+    struct Output {
+        let isDeleteSuccess: PublishSubject<Bool> = .init()
+    }
+    
+    private let repository: DataManagerRepository
     
     init(repository: DataManagerRepository) {
         self.repository = repository
     }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        bindModelDeleteEvent(input.modelDeleteEvent, to: output.isDeleteSuccess, disposeBag: disposeBag)
+        
+        return output
+    }
+}
+
+private extension DailyDeleteUseCase {
+    func bindModelDeleteEvent(_ modelDeleteEvent: Observable<DailyModel>, to isDeleteSuccess: PublishSubject<Bool>, disposeBag: DisposeBag) {
+        modelDeleteEvent
+            .subscribe(onNext: { [weak self] model in
+                guard let self else { return }
+                do {
+                    try self.execute(model: model) {
+                        isDeleteSuccess.onNext(true)
+                    }
+                } catch {
+                    isDeleteSuccess.onNext(false)
+                }
+            }).disposed(by: disposeBag)
+    }
+    
     
     func execute(model: DailyModel, _ completion: @escaping () -> Void) throws {
         try repository.delete(model)
--- Tempus/Presentation/DailyScene/ViewModel/DailyListViewModel.swift
+++ Tempus/Presentation/DailyScene/ViewModel/DailyListViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by 이정민 on 2023/04/28.
 //
 
+import RxCocoa
+import RxRelay
 import RxSwift
 
 final class DailyListViewModel {
@@ -15,7 +17,63 @@ final class DailyListViewModel {
     }
     
     struct Output {
-        let dailyModelArray: Observable<[DailyModel]>
-        let isDeleteSuccess: Observable<Bool>
+        let dailyModelArray: BehaviorSubject<[DailyModel]> = .init(value: [])
+        let isDeleteSuccess: PublishRelay<Bool> = .init()
+    }
+    
+    private var dailyFetchUseCase: DailyFetchUseCase
+    private var dailyDeleteUseCase: DailyDeleteUseCase
+    
+    private var modelFetchEvent: PublishSubject<Void>!
+    
+    init(repository: DataManagerRepository) {
+        self.dailyFetchUseCase = .init(repository: repository)
+        self.dailyDeleteUseCase = .init(repository: repository)
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        self.modelFetchEvent = input.modelFetchEvent
+        
+        let fetchUseCaseInput = DailyFetchUseCase.Input(modelFetchEvent: input.modelFetchEvent)
+        let fetchUseCaseOutput = dailyFetchUseCase.transform(input: fetchUseCaseInput,
+                                                             disposeBag: disposeBag)
+        
+        let deleteUseCaseInput = DailyDeleteUseCase.Input(modelDeleteEvent: input.modelDeleteEvent)
+        let deleteUseCaseOutput = dailyDeleteUseCase.transform(input: deleteUseCaseInput,
+                                                               disposeBag: disposeBag)
+        fetchUseCaseOutput.modelArrayObservable
+            .bind(to: output.dailyModelArray)
+            .disposed(by: disposeBag)
+        
+        bindDeleteSuccess(deleteUseCaseOutput.isDeleteSuccess, disposeBag)
+        bindAddButton(input.addButtonEvent, disposeBag: disposeBag)
+        
+        return output
+    }
+}
+
+private extension DailyListViewModel {
+    func bindDeleteSuccess(_ isDeleteSuccess: PublishSubject<Bool>, _ disposeBag: DisposeBag) {
+        isDeleteSuccess
+            .subscribe(onNext: { [weak self] isSuccess in
+                guard let self = self else { return }
+                if isSuccess {
+                    self.refresh()
+                }
+            }).disposed(by: disposeBag)
+    }
+    
+    func bindAddButton(_ addButtonEvent: Observable<Void>, disposeBag: DisposeBag) {
+        addButtonEvent
+            .subscribe(onNext: {
+                // coordinator push to createViewModel by 'push(fetchRefreshDelegate: self)' function
+            }).disposed(by: disposeBag)
+    }
+}
+
+extension DailyListViewModel: FetchRefreshDelegate {
+    func refresh() {
+        modelFetchEvent.onNext(())
     }
 }
