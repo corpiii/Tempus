f524df8996888c5ce7aec47b4e9abdbaf3070ab1
jjpush <lj845854@gmail.com>
2023-05-02 14:52:03 +0900

    refactor: DailyCreateUseCase작동방식을 Input, Output 으로 변경

--- Tempus/Domain/UseCase/Daily/DailyCreateUseCase.swift
+++ Tempus/Domain/UseCase/Daily/DailyCreateUseCase.swift
@@ -5,11 +5,47 @@
 //  Created by 이정민 on 2023/04/01.
 //
 
+import RxSwift
+
 final class DailyCreateUseCase {
-    let repository: DataManagerRepository
+    struct Input {
+        let modelCreate: Observable<DailyModel>
+    }
+    
+    struct Output {
+        let isCreateSuccess: PublishSubject<Bool> = .init()
+    }
+    
+    private let repository: DataManagerRepository
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        bindModelCreate(input.modelCreate, to: output.isCreateSuccess, disposeBag)
+        
+        return output
+    }
     
     init(repository: DataManagerRepository) {
         self.repository = repository
+    }
+}
+
+private extension DailyCreateUseCase {
+    func bindModelCreate(_ modelCreate: Observable<DailyModel>,
+                         to isCreateSuccess: PublishSubject<Bool>,
+                         _ disposeBag: DisposeBag) {
+        modelCreate
+            .subscribe(onNext: { [weak self] model in
+                guard let self = self else { return }
+                do {
+                    try self.execute(model: model) {
+                        isCreateSuccess.onNext(true)
+                    }
+                } catch {
+                    isCreateSuccess.onNext(false)
+                }
+            }).disposed(by: disposeBag)
     }
     
     func execute(model: DailyModel, _ completion: @escaping () -> Void) throws {
