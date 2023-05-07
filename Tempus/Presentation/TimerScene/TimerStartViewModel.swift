d2341931ace3a0c3951cd102bf0b61fe0bc45490
jjpush <lj845854@gmail.com>
2023-05-07 19:51:08 +0900

    feat: TimerStartviewModel Input, Output 정의 및 bind

--- Tempus/Presentation/TimerScene/TimerStartViewModel.swift
+++ Tempus/Presentation/TimerScene/TimerStartViewModel.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+import RxSwift
+
+final class TimerStartViewModel {
+    struct Input {
+        let modelWasteTime: Observable<Double>
+        
+        let startButtonTapEvent: Observable<Void>
+    }
+    
+    struct Output {
+        let isStartSuccess: PublishSubject<Bool> = .init()
+    }
+    
+    private var wasteTime: Double?
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        bindModelWasteTime(input.modelWasteTime, disposeBag)
+        
+        return output
+    }
+}
+
+private extension TimerStartViewModel {
+    func bindModelWasteTime(_ modelWasteTime: Observable<Double>, _ disposeBag: DisposeBag) {
+        modelWasteTime
+            .subscribe(onNext: { [weak self] wasteTime in
+                self?.wasteTime = wasteTime
+            }).disposed(by: disposeBag)
+    }
+    
+    func bindStartButtonTapEvent(_ startButtonTapEvent: Observable<Void>, _ disposeBag: DisposeBag) {
+        startButtonTapEvent
+            .subscribe(onNext: { [weak self] in
+                guard let wasteTime = self?.wasteTime else { return }
+                
+                let originModel = TimerModel(id: UUID(), wasteTime: wasteTime)
+                let startUseCase = TimerStartUseCase(originModel: originModel)
+                
+                // coordinator push with startUseCase
+            }).disposed(by: disposeBag)
+    }
+    
+}
