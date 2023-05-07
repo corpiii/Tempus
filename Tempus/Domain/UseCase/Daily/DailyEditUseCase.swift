873fbdb90482448713f9519fbb84ec540e808bdc
jjpush <lj845854@gmail.com>
2023-05-07 15:02:55 +0900

    refactor: DailyEditUseCase를 Input, Output을 통해 기능하도록 수정

--- Tempus/Domain/UseCase/Daily/DailyEditUseCase.swift
+++ Tempus/Domain/UseCase/Daily/DailyEditUseCase.swift
@@ -5,11 +5,46 @@
 //  Created by 이정민 on 2023/04/01.
 //
 
+import RxSwift
+
 final class DailyEditUseCase {
-    let repository: DataManagerRepository
+    struct Input {
+        let modelEditEvent: Observable<DailyModel>
+    }
+    
+    struct Output {
+        let isEditSuccess: PublishSubject<Bool> = .init()
+    }
+    
+    private let repository: DataManagerRepository
     
     init(repository: DataManagerRepository) {
         self.repository = repository
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        bindModelEditEvent(input.modelEditEvent, to: output.isEditSuccess, disposeBag: disposeBag)
+        
+        return output
+    }
+}
+
+private extension DailyEditUseCase {
+    func bindModelEditEvent(_ editEvent: Observable<DailyModel>,
+                            to isEditSuccess: PublishSubject<Bool>,
+                            disposeBag: DisposeBag) {
+        editEvent
+            .subscribe(onNext: { model in
+                do {
+                    try self.execute(model: model) {
+                        isEditSuccess.onNext(true)
+                    }
+                } catch {
+                    isEditSuccess.onNext(false)
+                }
+            }).disposed(by: disposeBag)
     }
     
     func execute(model: DailyModel, _ completion: @escaping () -> Void) throws {
