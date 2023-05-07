d4f47ca5a5b987b6632cc69753d65b1cc0249255
jjpush <lj845854@gmail.com>
2023-05-07 16:31:59 +0900

    test: DailyTimeDurationEditViewModelTest edit Test

--- TempusTests/ViewModelTests/DailySceneTests/EditTest/DailyTimeDurationEditViewModelTest.swift
+++ TempusTests/ViewModelTests/DailySceneTests/EditTest/DailyTimeDurationEditViewModelTest.swift
@@ -7,29 +7,81 @@
 
 import XCTest
 
+import RxSwift
+
 final class DailyTimeDurationEditViewModelTest: XCTestCase {
-
+    var repositoryMock: DataManagerRepositoryMock!
+    var disposeBag: DisposeBag!
+    
+    var fetchRefreshDelegate: FetchRefreshMock!
+    var editReflectDelegate: EditReflectMock!
+    
+    var originModel: DailyModel!
+    
+    var dailyTimeDurationEditViewModel: DailyTimeDurationEditViewModel!
+    
+    var startTime: PublishSubject<Double>!
+    var repeatCount: PublishSubject<Int>!
+    var completeButtonTapEvent: PublishSubject<Void>!
+    
+    var input: DailyTimeDurationEditViewModel.Input!
+    var output: DailyTimeDurationEditViewModel.Output!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        repositoryMock = .init()
+        disposeBag = .init()
+        
+        fetchRefreshDelegate = .init()
+        editReflectDelegate = .init()
+        
+        originModel = .init(id: UUID(),
+                            title: "testTitle",
+                            startTime: 13.0 * 60 * 60,
+                            repeatCount: 4,
+                            focusTime: 1.5 * 60 * 60,
+                            breakTime: 0.5 * 60 * 60)
+        
+        dailyTimeDurationEditViewModel = .init(originModel: originModel,
+                                               repository: repositoryMock,
+                                               fetchRefreshDelegate: fetchRefreshDelegate,
+                                               editReflectDelegate: editReflectDelegate)
+        
+        startTime = .init()
+        repeatCount = .init()
+        completeButtonTapEvent = .init()
+        
+        input = .init(startTime: startTime,
+                      repeatCount: repeatCount,
+                      backButtonTapEvent: PublishSubject<Void>(),
+                      completeButtonTapEvent: completeButtonTapEvent)
+        output = dailyTimeDurationEditViewModel.transform(input: input, disposeBag: disposeBag)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_edit_is_success() {
+        // Arrange
+        let expectation = XCTestExpectation(description: "edit_is_success")
+        let testStartTime = 15.0 * 60 * 60
+        let testRepeatCount = 3
+        var result = false
+        
+        // Act
+        output.isEditSuccess
+            .subscribe(onNext: { isSuccess in
+                result = isSuccess
+                expectation.fulfill()
+            }).disposed(by: disposeBag)
+        
+        startTime.onNext(testStartTime)
+        repeatCount.onNext(testRepeatCount)
+        completeButtonTapEvent.onNext(())
+        
+        // Assert
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertTrue(result)
+        
+        let dailyModel = repositoryMock.dailyModel
+        XCTAssertEqual(dailyModel?.startTime, testStartTime)
+        XCTAssertEqual(dailyModel?.repeatCount, testRepeatCount)
     }
 
 }
