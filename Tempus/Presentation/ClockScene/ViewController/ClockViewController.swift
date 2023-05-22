e969c52275098128d5779097bdcb1aa0aecf6344
jjpush <lj845854@gmail.com>
2023-05-22 23:15:38 +0900

    feat: ClockViewController 내 button Stack 구현

--- Tempus/Presentation/ClockScene/ViewController/ClockViewController.swift
+++ Tempus/Presentation/ClockScene/ViewController/ClockViewController.swift
@@ -15,8 +15,27 @@ class ClockViewController: UIViewController {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("시작", for: .normal)
+        button.setTitleColor(UIColor(displayP3Red: 100, green: 0, blue: 100, alpha: 1), for: .normal)
         
         return button
+    }()
+    
+    private let stopButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("중지", for: .normal)
+        button.setTitleColor(UIColor(displayP3Red: 0, green: 100, blue: 100, alpha: 1), for: .normal)
+        
+        return button
+    }()
+    
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        
+        return stackView
     }()
     
     private let countDownTimerView: CountDownTimerView = .init()
@@ -46,6 +65,7 @@ private extension ClockViewController {
         configureTimerView()
         
         // 시작버튼
+        configureButtonStackView()
     }
     
     func configureTimerView() {
@@ -58,7 +78,20 @@ private extension ClockViewController {
             make.trailing.equalTo(safeArea.snp.trailing)
             
             make.height.equalTo(countDownTimerView.snp.width)
-            make.centerY.equalTo(safeArea.snp.centerY)
+            make.top.equalTo(safeArea.snp.top).inset(self.view.bounds.height * 0.05)
+        }
+    }
+    
+    func configureButtonStackView() {
+        self.view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(startButton)
+        buttonStackView.addArrangedSubview(stopButton)
+        
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        buttonStackView.snp.makeConstraints { make in
+            make.top.equalTo(countDownTimerView.snp.bottom).offset(30)
+            make.centerX.equalTo(safeArea.snp.centerX)
         }
     }
 }
@@ -82,6 +115,8 @@ private extension ClockViewController {
                 
                 output.remainTime.subscribe(onNext: { time in
                     // 받은 time을 뷰에다 넣기
+                    print(time)
+                    self.countDownTimerView.setTime(time)
                 }).disposed(by: self.disposeBag)
             }).disposed(by: disposeBag)
     }
