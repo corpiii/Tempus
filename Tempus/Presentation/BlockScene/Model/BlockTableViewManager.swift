bffc68818e25721547c3753fe4496570e39ea497
jjpush <lj845854@gmail.com>
2023-06-01 16:34:47 +0900

    refactor: BlockTableViewManager를 생성해서 TableViewDiffableDataSource를 분리
    - 뷰 로직을 분리해서 viewController를 경량화 하기 위함

---
+++ Tempus/Presentation/BlockScene/Model/BlockTableViewManager.swift
@@ -0,0 +1,69 @@
+//
+//  BlockTableViewManager.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/06/01.
+//
+
+import UIKit
+
+protocol TableViewManager {
+    associatedtype Section: Hashable
+    associatedtype Model: Hashable
+    
+    var dataSource: UITableViewDiffableDataSource<Section, Model> { get set }
+    
+    func apply(section: Section, models: [Model])
+    func delete(model: Model)
+    func fetch(index: Int) -> Model
+}
+
+struct BlockTableViewManager: TableViewManager {
+    typealias Model = BlockModel
+    
+    enum Section {
+        case main
+    }
+
+    var dataSource: UITableViewDiffableDataSource<Section, BlockModel>
+
+    init(tableView: UITableView) {
+        dataSource = .init(tableView: tableView, cellProvider: { tableView, indexPath, model in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: BlockCell.identifier, for: indexPath) as? BlockCell else {
+                return UITableViewCell()
+            }
+
+            cell.modelTitleLabel.text = model.title
+            return cell
+        })
+
+        tableView.dataSource = dataSource
+
+        var snapShot = NSDiffableDataSourceSnapshot<Section, BlockModel>()
+        snapShot.appendSections([.main])
+        dataSource.apply(snapShot)
+
+    }
+
+    func apply(section: Section, models: [BlockModel]) {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, BlockModel>()
+        snapShot.appendSections([section])
+        snapShot.appendItems(models)
+
+        dataSource.apply(snapShot)
+    }
+
+    func delete(model: BlockModel) {
+        var snapshot = self.dataSource.snapshot()
+        snapshot.deleteItems([model])
+
+        self.dataSource.apply(snapshot)
+    }
+
+    func fetch(index: Int) -> BlockModel {
+        let snapShot = dataSource.snapshot()
+        let model = snapShot.itemIdentifiers[index]
+
+        return model
+    }
+}
