e353166a0c33b01134feca41d904bbc19d865c85
jjpush <lj845854@gmail.com>
2023-06-07 16:16:36 +0900

    feat: DailyTimeDurationViewController 뷰 정의

---
+++ Tempus/Presentation/DailyScene/ViewController/DailyTimeDurationViewController.swift
@@ -0,0 +1,83 @@
+//
+//  DailyTimeDurationViewController.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/06/07.
+//
+
+import UIKit
+
+final class DailyTimeDurationViewController: UIViewController {
+    private let doneBarButton: UIBarButtonItem = .init(systemItem: .done)
+    
+    // TODO: clockView
+ 
+    private let entireStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private let startTimeStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private let startTimeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let startTimePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        
+        return picker
+    }()
+    
+    private let repeatCountStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        
+        return stackView
+    }()
+    
+    private let repeatCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let repeatCountStepper: UIStepper = {
+        let stepper = UIStepper()
+        stepper.translatesAutoresizingMaskIntoConstraints = false
+        
+        return stepper
+    }()
+    
+    private weak var viewModel: DailyTimeDurationEditViewModel?
+    
+    init(viewModel: DailyTimeDurationEditViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
