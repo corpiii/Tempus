6cf99487424e8afc73779b2b0d88733c858daed1
jjpush <lj845854@gmail.com>
2023-06-14 11:23:30 +0900

    feat: DailyCell 정의

--- Tempus/Presentation/DailyScene/View/DailyCell.swift
+++ Tempus/Presentation/DailyScene/View/DailyCell.swift
@@ -7,17 +7,37 @@
 
 import UIKit
 
+import SnapKit
+
 final class DailyCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let modelTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+}
 
+private extension DailyCell {
+    func configureUI() {
+        self.addSubview(modelTitleLabel)
+        
+        modelTitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.snp.leading).inset(30)
+            make.trailing.equalTo(self.snp.trailing).inset(30)
+            
+            make.top.equalTo(self.snp.top)
+            make.bottom.equalTo(self.snp.bottom)
+        }
+    }
 }
