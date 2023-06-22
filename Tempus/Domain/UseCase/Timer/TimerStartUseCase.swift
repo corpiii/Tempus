dfbbf65877785ef77df4763a93649f624c303396
jjpush <lj845854@gmail.com>
2023-06-22 13:10:01 +0900

    feat: Block Timer 실행 시 Local Notification 등록
    - 제거할 때 받은 알림도 모두 삭제

--- Tempus/Domain/UseCase/Timer/TimerStartUseCase.swift
+++ Tempus/Domain/UseCase/Timer/TimerStartUseCase.swift
@@ -106,5 +106,6 @@ private extension TimerStartUseCase {
     
     func removeNotification() {
         UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
 }
