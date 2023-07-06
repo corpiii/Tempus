93229389ce19e502ca6df5f95d08ac182cbf834f
jjpush <lj845854@gmail.com>
2023-07-06 19:34:29 +0900

    feat: 전체 앱에서 사용되는 색깔을 관리하는 ColorConstant 생성

---
+++ Tempus/Presentation/Common/Model/ColorConstant.swift
@@ -0,0 +1,15 @@
+//
+//  ColorConstant.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/07/06.
+//
+
+import UIKit
+
+enum ColorConstant {
+    static let firstColor: UIColor = .init(hex: "#A0C49D")!
+    static let secondColor: UIColor = .init(hex: "#C4D7B2")!
+    static let thirdColor: UIColor = .init(hex: "#E1ECC8")!
+    static let fourthColor: UIColor = .init(hex: "#FFFFFF")!
+}
