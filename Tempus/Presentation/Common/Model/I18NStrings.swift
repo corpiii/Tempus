25edbc34af6bfc394ea6ae6a965320c5793038f3
jjpush <lj845854@gmail.com>
2023-07-18 08:15:16 +0900

    feat: Localized를 위한 I18NStrings 파일 및 Localizable 파일 생성

---
+++ Tempus/Presentation/Common/Model/I18NStrings.swift
@@ -0,0 +1,50 @@
+//
+//  I18NStrings.swift
+//  Tempus
+//
+//  Created by 이정민 on 2023/07/18.
+//
+
+import Foundation
+
+enum I18NStrings {
+    enum Alert {
+        static let alertTitle = "alertTitle".localized
+        static let alertFailTitle = "alertFailTitle".localized
+        static let successCreateTitle = "successCreateTitle".localized
+        static let storeAlertTitle = "storeAlertTitle".localized
+        
+        static let storeAlertMessage = "storeAlertMessage".localized
+        static let timerStartNowMessage = "timerStartNowMessage".localized
+        static let modeEmptyMessage = "modeEmptyMessage".localized
+        static let dataEmptyCheckMessage = "dataEmptyCheckMessage".localized
+        
+        static let confirmAction = "confirmAction".localized
+        static let deleteAction = "deleteAction".localized
+        static let YesAction = "YesAction".localized
+        static let NoAction = "NoAction".localized
+    }
+    
+    enum NavigationItem {
+        static let blockListTitle = "blockListTitle".localized
+        static let dailyListTitle = "dailyListTitle".localized
+        
+        static let create = "create".localized
+        static let edit = "edit".localized
+        static let next = "next".localized
+        static let start = "start".localized
+    }
+    
+    enum View {
+        static let timeInterval = "timeInterval".localized
+        static let titlePlaceholder = "titlePlaceholder".localized
+        static let select = "select".localized
+    }
+    
+    enum DataManageError {
+        static let createFailure = "createFailure".localized
+        static let fetchFailure = "fetchFailure".localized
+        static let updateFailure = "updateFailure".localized
+        static let deleteFailure = "deleteFailure".localized
+    }
+}
