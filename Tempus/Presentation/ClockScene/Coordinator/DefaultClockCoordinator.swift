d2eb393e9252b86d182ca9953525b2c9dcccf2b9
jjpush <lj845854@gmail.com>
2023-08-25 13:38:01 +0900

    refactor: ClockScene의 DefaultViewModel이 ViewModel protocol을 의존하도록 수정

--- Tempus/Presentation/ClockScene/Coordinator/DefaultClockCoordinator.swift
+++ Tempus/Presentation/ClockScene/Coordinator/DefaultClockCoordinator.swift
@@ -13,7 +13,7 @@ class DefaultClockCoordinator: ClockCoordinator {
     
     private weak var startApplyDelegate: StartApplyDelegate?
     
-    let clockViewModel: ClockViewModel
+    let clockViewModel: DefaultClockViewModel
     let clockViewController: ClockViewController
     
     init(startApplyDelegate: StartApplyDelegate) {
@@ -21,7 +21,7 @@ class DefaultClockCoordinator: ClockCoordinator {
         let tabBarSelectedImage = UIImage(systemName: "clock.fill")
         let tabBarItem = UITabBarItem(title: nil, image: tabBarImage, selectedImage: tabBarSelectedImage)
         
-        self.clockViewModel = ClockViewModel()
+        self.clockViewModel = DefaultClockViewModel()
         self.clockViewController = ClockViewController(viewModel: self.clockViewModel)
         self.clockViewController.tabBarItem = tabBarItem
         self.startApplyDelegate = startApplyDelegate
--- Tempus/Presentation/ClockScene/ViewModel/ClockViewModel.swift
+++ Tempus/Presentation/ClockScene/ViewModel/ClockViewModel.swift
@@ -2,84 +2,14 @@
 //  ClockViewModel.swift
 //  Tempus
 //
-//  Created by 이정민 on 2023/04/23.
+//  Created by 이정민 on 2023/08/24.
 //
 
-import Foundation
-
 import RxSwift
-import UserNotifications
 
-final class ClockViewModel {
-    struct Input {
-        let modeStartEvent: PublishSubject<Void>
-        let modeStopEvent: PublishSubject<Void>
-    }
+protocol ClockViewModel: AnyObject, StartModeDelegate {
+    var modeStartUseCase: ModeStartUseCase? { get set }
+    var coordinator: ClockCoordinator? { get set }
     
-    struct Output {
-        let modeStartUseCaseOutput: PublishSubject<ModeStartUseCase.Output>
-    }
-    
-    var modeStartUseCase: ModeStartUseCase? {
-        didSet {
-            guard let modeStartUseCase else { return }
-            
-            let startUseCaseInput = ModeStartUseCase.Input(modeStartEvent: self.modeStartEvent,
-                                                           modeStopEvent: self.modeStopEvent)
-            let startUseCaseOutput = modeStartUseCase.transform(input: startUseCaseInput, disposeBag: self.disposeBag)
-            
-            self.modeStartUseCaseOutput.onNext(startUseCaseOutput)
-        }
-    }
-    
-    init() {
-        NotificationCenter.default.addObserver(self, selector: #selector(observeModel), name: NSNotification.Name("modelNotification"), object: nil)
-    }
-    
-    @objc private func observeModel(_ sender: Notification) {
-        if let object = sender.object as? Data {
-            let decoder = JSONDecoder()
-            
-            if let dailyModel = try? decoder.decode(DailyModel.self, from: object) {
-                let dailyStartUseCase = DailyStartUseCase(originModel: dailyModel)
-                self.modeStartUseCase = dailyStartUseCase
-                self.modeStartEvent.onNext(())
-            } else if let blockModel = try? decoder.decode(BlockModel.self, from: object) {
-                let blockStartUseCase = BlockStartUseCase(originModel: blockModel)
-                self.modeStartUseCase = blockStartUseCase
-                self.modeStartEvent.onNext(())
-            } else if let timerModel = try? decoder.decode(TimerModel.self, from: object) {
-                let timerStartUseCase = TimerStartUseCase(originModel: timerModel)
-                self.modeStartUseCase = timerStartUseCase
-            }
-        }
-    }
-    
-    private var modeStartEvent: PublishSubject<Void> = .init()
-    private var modeStopEvent: PublishSubject<Void> = .init()
-    private var disposeBag: DisposeBag = .init()
-    
-    private let modeStartUseCaseOutput: PublishSubject<ModeStartUseCase.Output> = .init()
-    weak var coordinator: ClockCoordinator?
-    
-    func transform(input: Input, disposeBag: DisposeBag) -> Output {
-        let output = Output(modeStartUseCaseOutput: modeStartUseCaseOutput)
-        
-        self.modeStartEvent = input.modeStartEvent
-        self.modeStopEvent = input.modeStopEvent
-        self.disposeBag = disposeBag
-        
-        return output
-    }
-}
-
-extension ClockViewModel: StartModeDelegate {
-    func startWith(_ startUseCase: ModeStartUseCase) {
-        self.modeStartUseCase = startUseCase
-        self.modeStartEvent.onNext(())
-        let date = Date().timeIntervalSince1970
-        UserDefaults.standard.set(date, forKey: "date")
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound], completionHandler: { _, _ in })
-        self.coordinator?.startTimer()
-    }
+    func transform<InputType, OutputType>(input: InputType, disposeBag: DisposeBag) -> OutputType?
 }
